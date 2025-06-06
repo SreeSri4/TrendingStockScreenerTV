0698b14c0c517457e6e5923dfa004c3658323ef2
Sreekanth <sreekanth.seelam@gmail.com>
2025-06-06 21:57:53 +0530

    Updates

--- index.js
+++ index.js
@@ -862,7 +862,8 @@ var vite_config_default = defineConfig({
   root: path.resolve(import.meta.dirname, "client"),
   base: "/TrendingStockScreenerTV/",
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: "dist",
+    //path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true
   },
   server: {
