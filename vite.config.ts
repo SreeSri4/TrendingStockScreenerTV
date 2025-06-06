24c275d88eb126cb42b78040fe9cf6950cfdd9a2
Sreekanth <sreekanth.seelam@gmail.com>
2025-06-07 00:39:18 +0530

    commit_test

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
+  plugins: [react() ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -23,12 +12,7 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
-  base: '/TrendingStockScreenerTV/',
-  build: {
-    outDir: 'dist', //path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+  base: '/TrendingStockScreenerTV',
   server: {
     fs: {
       strict: true,
